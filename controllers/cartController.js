d34a7cab408863643fb71364c5c5665470119d61
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-08-30 11:32:06 +0100

    added functions to delete item or reduce cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -33,7 +33,72 @@ const getCart = async (req, res) => {
 }
 // END OF FUNCTION TO GET CART
 
+// FUNCTION TO ADD PRODUCT TO CART
 const addToCart = async (req, res) => {
+  try {
+    // DESTRUCTURE REQUIRED VALUES FROM REQ.BODY
+    const { userId, cartItem, quantity } = req?.body
+
+    // VALIDATE REQUIRED VALUES
+    if (!userId || !cartItem) {
+      res.status(400).json('Please pass all required details')
+    }
+
+    // FIND USER
+    const user = await User.findById(userId)
+
+    // CHECK IF USER EXISTS
+    if (!user) {
+      res.status(404).json('User not found')
+    }
+
+    // CHECK IF PRODUCT EXISTS
+    const foundProduct = await Product.findOne({ _id: cartItem })
+
+    //   IF PRODUCT WAS NOT FOUND
+    if (!foundProduct) {
+      res.status(404).json('Product not found')
+    } else {
+      // IF PRODUCT AND USER EXISTS, CHECK IF THE PRODUCT TO BE ADDED ALREADY EXISTS IN THE CART
+
+      // FIND CART
+      const cart = await Cart.findOne({ userId })
+
+      if (cart) {
+        // IF USER HAS A CART, CHECK IF THE ITEM TO BE ADDED ALREADY EXISTS IN THE CART
+        const existingProduct = cart?.products?.find(
+          (product) => product?.cartItem?.toString() === cartItem
+        )
+
+        // IF PRODUCT TO BE ADDED ALREADY EXISTS IN THE CART
+        if (existingProduct) {
+          existingProduct.quantity += quantity
+        } else {
+          // IF PRODUCT TO BE ADDED DOES NOT ALREADY EXIST IN THE CART
+          cart?.products?.push({ cartItem, quantity })
+        }
+
+        await cart.save()
+        res.status(200).json({ message: 'Product added to cart', data: cart })
+      } else {
+        // CREATE A NEW CART INSTANCE IF THE USER'S CART IS EMPTY
+        const newCart = new Cart({
+          userId,
+          products: [{ cartItem, quantity: quantity }],
+        })
+
+        await newCart.save()
+        res.status(200).json('Product added to cart')
+      }
+    }
+  } catch (error) {
+    res.status(500).json('An unexpected error occurred. Please try again.')
+  }
+}
+// END OF FUNCTION TO ADD PRODUCT TO CART
+
+// FUNCTION TO REMOVE PRODUCT FROM CART
+const removeFromCart = async (req, res) => {
   try {
     // DESTRUCTURE REQUIRED VALUES FROM REQ.BODY
     const { userId, cartItem, quantity } = req?.body
@@ -59,37 +124,84 @@ const addToCart = async (req, res) => {
       res.status(404).json('Product not found')
     }
 
-    // IF PRODUCT AND USER EXISTS, CHECK IF THE PRODUCT TO BE ADDED ALREADY EXISTS IN THE CART
+    // IF PRODUCT AND USER EXISTS, CHECK IF THE PRODUCT TO BE REMOVED ALREADY EXISTS IN THE CART
 
     // FIND CART
     const cart = await Cart.findOne({ userId })
 
     if (cart) {
-      // IF USER HAS A CART, CHECK IF THE ITEM TO BE ADDED ALREADY EXISTS IN THE CART
+      // IF USER HAS A CART, CHECK IF THE ITEM TO BE REMOVED ALREADY EXISTS IN THE CART
       const existingProduct = cart?.products?.find(
         (product) => product?.cartItem?.toString() === cartItem
       )
 
+      // IF PRODUCT TO BE REMOVED ALREADY EXISTS IN THE CART
       if (existingProduct) {
-        existingProduct.quantity += quantity
+        if (existingProduct.quantity === 1) {
+          await Cart.updateOne(
+            { userId: userId },
+            { $pull: { products: { cartItem } } }
+          )
+          res.status(200).json('Product removed from cart')
+        } else {
+          existingProduct.quantity -= 1
+          await cart.save()
+          res.status(200).json('Unit removed from cart')
+        }
       } else {
-        cart?.products?.push({ cartItem, quantity })
+        // IF PRODUCT TO BE REMOVED DOES NOT ALREADY EXIST IN THE CART
+        res.status(404).json('Product not found')
       }
-
-      await cart.save()
-      res.status(200).json({ message: 'Product added to cart', data: cart })
     } else {
-      const newCart = new Cart({
-        userId,
-        products: [{ cartItem, quantity: quantity }],
-      })
-
-      await newCart.save()
-      res.status(200).json('Product added to cart')
+      res.status(404).json('Cart not found')
     }
   } catch (error) {
     res.status(500).json('An unexpected error occurred. Please try again.')
   }
 }
+// END OF FUNCTION TO REMOVE PRODUCT FROM CART
 
-module.exports = { getCart, addToCart }
+// FUNCTION TO DELETE PRODUCT FROM CART
+const deleteFromCart = async (req, res) => {
+  try {
+    // GET USERID FROM PARAMS
+    const { cartId, userId, cartItemId } = req?.body
+
+    // VALIDATE USERID
+    if (!cartId || !userId || !cartItemId) {
+      res
+        .status(500)
+        .json('Kindly confirm it cartID, cartItemId and userID are present')
+    }
+
+    // CHECK IF USER AND CART EXISTS
+    const [cart, user] = await Promise.all([
+      Cart.findOne({ userId }),
+      User.findById(userId),
+    ])
+
+    if (!user || !cart) {
+      return res.status(404).json({ error: 'User or Cart not found' })
+    }
+
+    // IF USER HAS A CART, CHECK IF THE ITEM TO BE REMOVED ALREADY EXISTS IN THE CART
+    const existingProduct = cart?.products?.find((product) => {
+      return product?.cartItem?.toString() === cartItemId
+    })
+
+    if (!existingProduct) {
+      res.status(404).json('Product not found in the cart')
+    } else {
+      await Cart.updateOne(
+        { userId: userId },
+        { $pull: { products: { cartItem: cartItemId } } }
+      )
+      res.status(200).json('Product removed from cart')
+    }
+  } catch (error) {
+    res.status(500).json('An unexpected error occurred. Please try again.')
+  }
+}
+// END OF FUNCTION TO DELETE PRODUCT FROM CART
+
+module.exports = { getCart, addToCart, removeFromCart, deleteFromCart }
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const { getCart, addToCart } = require('../controllers/cartController')
+const {
+  getCart,
+  addToCart,
+  removeFromCart,
+  deleteFromCart,
+} = require('../controllers/cartController')
 
 const router = express.Router()
 
 router.get('/', getCart)
 router.post('/addToCart', addToCart)
+router.post('/removeFromCart', removeFromCart)
+router.post('/deleteFromCart', deleteFromCart)
 
 module.exports = router
